4dbc77ff0b4f546716ff98156a1944b960526a55
puifaiba <puifai.ba@gmail.com>
2021-01-27 11:01:27 -0500

    commit tree 65c27faafca1a2723ae4c08a3d67ad66c190e7cd

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,21 @@
 def clock_angle(time)
-    # code your solution here
+
+    time_array = time.split(":")
+    hour = time_array[0].to_i
+    minute = time_array[1].to_i
+    hourDegree = 30 * hour
+    minuteDegree = 6 * minute
+
+    if minute == 12
+        minute = 0
+    end
+
+    if hourDegree < minuteDegree
+        angle = 360 - minuteDegree + hourDegree
+    else 
+        angle = hourDegree - minuteDegree
+    end
+    angle
 end
+
+
